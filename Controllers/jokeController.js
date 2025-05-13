9896a4c9fc655567ff7d3e4ba20515942e5642fa
Yohan <150443604+yohan59235@users.noreply.github.com>
2025-05-13 18:29:43 +0200

    create function addJoke in jokecontroller.js

--- Controllers/jokeController.js
+++ Controllers/jokeController.js
@@ -1,2 +1,11 @@
-const { Joke } = require('../models');
+const { Joke } = require("../models");
 
+exports.addJoke = async (req, res) => {
+  try {
+    const { question, reponse } = req.body;
+    const joke = await Joke.create({ question, reponse });
+    res.status(201).json(joke);
+  } catch (err) {
+    res.status(500).json({ error: "Erreur lors de l ajout" });
+  }
+};
