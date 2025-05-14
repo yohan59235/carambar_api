b614ccb3995d301c55983c7bea91e7fdb3f796a3
Yohan <150443604+yohan59235@users.noreply.github.com>
2025-05-14 17:49:56 +0200

    create route for getJokeByCategorie

--- routes/jokesRoutes.js
+++ routes/jokesRoutes.js
@@ -6,5 +6,6 @@ router.post("/", controller.addJoke);
 router.get("/blague", controller.getAllJokes);
 router.get("/blague/:id", controller.getJokeById);
 router.get("/blague/random", controller.getRandomJoke);
+router.get("/categorie/:nom", controller.getJokesByCategorie);
 
 module.exports = router;
