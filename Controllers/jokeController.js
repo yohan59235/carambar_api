4dd1a33a289bf3f38551e4ed3a5729b24c73a3f7
Yohan <150443604+yohan59235@users.noreply.github.com>
2025-05-14 16:31:03 +0200

    create function for show all jokes

--- Controllers/jokeController.js
+++ Controllers/jokeController.js
@@ -2,10 +2,19 @@ const { Joke } = require("../models");
 
 exports.addJoke = async (req, res) => {
   try {
-    const { question, reponse } = req.body;
+    const { categorie, question, reponse } = req.body;
     const joke = await Joke.create({ categorie, question, reponse });
     res.status(201).json(joke);
   } catch (err) {
     res.status(500).json({ error: "Erreur lors de l ajout" });
+  }
+};
+
+exports.getAllJokes = async (req, res) => {
+  try {
+    const jokes = await Joke.findAll();
+    res.json(jokes);
+  } catch (err) {
+    res.status(500).json({ error: "Erreur de la récupération des blagues" });
   }
 };
