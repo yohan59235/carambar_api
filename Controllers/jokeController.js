2c091ee5813df022252a404d0c60ea7de0cd3eca
Yohan <150443604+yohan59235@users.noreply.github.com>
2025-05-14 15:48:08 +0200

    add categorie on database

--- Controllers/jokeController.js
+++ Controllers/jokeController.js
@@ -3,7 +3,7 @@ const { Joke } = require("../models");
 exports.addJoke = async (req, res) => {
   try {
     const { question, reponse } = req.body;
-    const joke = await Joke.create({ question, reponse });
+    const joke = await Joke.create({ categorie, question, reponse });
     res.status(201).json(joke);
   } catch (err) {
     res.status(500).json({ error: "Erreur lors de l ajout" });
--- models/joke.js
+++ models/joke.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/database");
 
 const Joke = sequelize.define("Joke", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  categorie: { type: DataTypes.STRING, allowNull: false },
   question: { type: DataTypes.STRING, allowNull: false },
   reponse: { type: DataTypes.STRING, allowNull: false },
 });
