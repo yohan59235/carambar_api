550bd653fbf5bce7f787485ecbc40034366383e2
Yohan <150443604+yohan59235@users.noreply.github.com>
2025-05-16 18:22:19 +0200

    add swagger in app.js

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const jokeRoutes = require("./routes/jokesRoutes");
 const { initDb } = require("./models");
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./swagger/swagger.yaml');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/blagues, jokeRoutes");
+app.use("/blagues", jokeRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 initDb();
 
