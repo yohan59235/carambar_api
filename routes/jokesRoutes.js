285dec55d7df12ac96ad6259e94aad57da05dd82
Yohan <150443604+yohan59235@users.noreply.github.com>
2025-05-16 18:18:43 +0200

    add content on swagger.yaml

--- routes/jokesRoutes.js
+++ routes/jokesRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../Controllers/jokeController");
 
-router.post("/", controller.addJoke);
+router.post("/blague", controller.addJoke);
 router.get("/blague", controller.getAllJokes);
 router.get("/blague/:id", controller.getJokeById);
 router.get("/blague/random", controller.getRandomJoke);
